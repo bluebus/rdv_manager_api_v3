ad3ae259f6bd41c4fe18257fbfc7d1151d034b19
unknown <cwyen1991@gmail.com>
2026-06-14 17:15:59 +0800

    Updated yml and minor change on class

--- src/main/java/com/grey/rdv_manager_api/security/SecurityConfig.java
+++ src/main/java/com/grey/rdv_manager_api/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.authentication.ProviderManager;
 
 /**
  * Master Spring Security configuration for the application.
